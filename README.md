0a264599c13db8e2f17b572b3bb6581802b6cf43
RadimBaca <radim.baca@vsb.cz>
2024-02-26 15:57:29 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# chat
+# Chat App
+
+Tato applikace slouží jako modelová aplikace v [kroužku programování na VŠB](kpostrava.github.io).
+
+Udělejte fork tohoto repository.
---
+++ server/main.js
@@ -0,0 +1,18 @@
+import { Server } from "socket.io";
+
+const io = new Server(3000, { cors: { origin: '*' } });
+
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('chat_message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat_message', msg);
+  });
+});
+
